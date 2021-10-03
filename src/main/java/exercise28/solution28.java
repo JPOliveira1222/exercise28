14868ed27b5355dd73e213a057194cc8603c9716
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:40:08 -0400

    pseudocode for 28

---
+++ src/main/java/exercise28/solution28.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 28 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise28;
+
+import java.util.Scanner;
+
+public class solution28 {
+    /*
+    Print "Enter a number:"
+
+    using a for loop get 5 numbers from user input and add them together
+
+    print "The total is " +sum
+     */
+
+
+    
+}
